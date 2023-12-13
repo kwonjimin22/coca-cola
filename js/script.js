58c57b1725c9e6715ca8ff10f35dede460a4b970
jimin <jimin00study@gmail.com>
2023-12-13 12:23:36 +0900

    슬라이드 추가 중

--- js/script.js
+++ js/script.js
@@ -9,4 +9,28 @@ $(function () {
       $header.addClass('hide');
     }
   });
+
+  // 슬라이더 플러그인 추가
+  const pList = new Swiper('.p-list', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+
+    // // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.p-btn .btn-next',
+      prevEl: '.p-btn .btn-prev',
+    },
+
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
 });
